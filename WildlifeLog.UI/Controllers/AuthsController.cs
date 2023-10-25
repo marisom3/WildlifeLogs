5799ce3168dd0bd63345ba41ef2edd6d26e88789
marisom3 <78823563+marisom3@users.noreply.github.com>
2023-10-24 19:38:56 -0700

    added logout button

--- WildlifeLog.UI/Controllers/AuthsController.cs
+++ WildlifeLog.UI/Controllers/AuthsController.cs
@@ -100,7 +100,39 @@ namespace WildlifeLog.UI.Controllers
 			return View();
 		}
 
-		
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			var client = httpClientFactory.CreateClient();
+
+
+			//create httpRequestMessage
+			var httpRequestMessage = new HttpRequestMessage()
+			{
+				Method = HttpMethod.Post,
+				RequestUri = new Uri("https://localhost:7075/api/auth/logout"),
+				
+			};
+
+
+			//use cleint to send httpRequestMessage to api and we get a json response abck 
+			var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+
+			//Ensure success 
+			httpResponseMessage.EnsureSuccessStatusCode();
+
+			//"Read the body" as as tring DONT Convert form JSON to Dto 
+			var response = await httpResponseMessage.Content.ReadAsStringAsync();
+
+			//If successful, redirect to ? 
+			if (response != null)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			//else just return to view 
+			return RedirectToAction("Login");
+		}
 
 		
 	}
