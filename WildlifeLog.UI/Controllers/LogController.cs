e5476ffc7ed8f49bd34f564179950c7af1688909
marisom3 <78823563+marisom3@users.noreply.github.com>
2023-10-24 22:19:45 -0700

    added Get All Logs to the Log Controller and dsiplayed them in tteh view

--- WildlifeLog.UI/Controllers/LogController.cs
+++ WildlifeLog.UI/Controllers/LogController.cs
@@ -1,14 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WildlifeLog.UI.Models.DTO;
 
 namespace WildlifeLog.UI.Controllers
 {
 	public class LogController : Controller
 	{
-		public IActionResult Index()
-		{
-			return View();
-		} 
+		private readonly IHttpClientFactory httpClientFactory;
 
+		public LogController(IHttpClientFactory httpClientFactory)
+		{
+			this.httpClientFactory = httpClientFactory;
+		}
+		
+
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			//create list of type ParkDto 
+			List<LogDto> logs = new List<LogDto>();
+
+			try
+			{
+				//create client 
+				var client = httpClientFactory.CreateClient();
+
+				//use client to talk to the API + get back all the park info 
+				var httpResponseMessage = await client.GetAsync("https://localhost:7075/api/log");
+
+				//EnsureSuccess 
+				httpResponseMessage.EnsureSuccessStatusCode();
+
+				//Convert the JSON data to a list of ParkDto and add to the parks list 
+				logs.AddRange(await httpResponseMessage.Content.ReadFromJsonAsync<IEnumerable<LogDto>>());
+			}
+			catch (Exception ex)
+			{
+				//log exception 
+			}
+
+			//retrun the list of parkDto to teh View 
+			return View(logs);
+		}
 
 	}
 }
